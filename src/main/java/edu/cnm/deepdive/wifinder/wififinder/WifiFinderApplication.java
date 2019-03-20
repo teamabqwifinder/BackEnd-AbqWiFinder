fc17be07fd0c755cef727d4086c5a39d81ed6eb3
fbeji <fbeji@unm.edu>
2019-03-20 17:01:12 -0600

    Initial commit

---
+++ src/main/java/edu/cnm/deepdive/wifinder/wififinder/WifiFinderApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.wifinder.wififinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiFinderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WifiFinderApplication.class, args);
+  }
+
+}
