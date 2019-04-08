75ff48b5a649eb0a51b398fc3121b4a540bc46d6
Chris <chris@speedycomputing.com>
2019-04-08 15:30:30 -0600

    Mvc Test

--- src/test/java/edu/cnm/deepdive/wifinder/wififinder/WifiFinderApplicationTests.java
+++ src/test/java/edu/cnm/deepdive/wifinder/wififinder/WifiFinderApplicationTests.java
@@ -1,17 +1,33 @@
 package edu.cnm.deepdive.wifinder.wififinder;
 
 
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class WifiFinderApplicationTests {
 
+  @Autowired
+  private MockMvc mockMvc;
+
   @Test
-  public void contextLoads() {
+  public void shouldReturnDefaultMessage() throws Exception {
+    //Make sure at least one location is returned, by checking that "id" is part of the response
+    this.mockMvc.perform(get("/locations")).andDo(print()).andExpect(status().isOk())
+            .andExpect(content().string(containsString("id")));
   }
 
 }
