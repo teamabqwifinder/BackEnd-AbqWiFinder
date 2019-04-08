50e8462d5a3b7e237bafc2b2f6757283ed82104d
Chris <chris@speedycomputing.com>
2019-04-08 15:17:40 -0600

    Fix test

--- src/test/java/edu/cnm/deepdive/wifinder/wififinder/WifiFinderApplicationTests.java
+++ src/test/java/edu/cnm/deepdive/wifinder/wififinder/WifiFinderApplicationTests.java
@@ -1,7 +1,7 @@
 package edu.cnm.deepdive.wifinder.wififinder;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
